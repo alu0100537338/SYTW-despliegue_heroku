92800b0216732a873642d4a13ce15af719da3d39
Jessica Ramos <audiken_92@hotmail.com>
2013-10-28 21:16:43 +0000

    Modificando

--- rock_paper.rb
+++ rock_paper.rb
@@ -6,7 +6,6 @@ builder = Rack:: Builder.new do
 	use Rack::Lint
 	use Rack::Session::Cookie,
   	{:key => 'rack.session',
-    :domain => 'example.com',
     :secret => 'cookie',
     :expire_after => 30}
 
@@ -16,8 +15,7 @@ end
 use Rack::Server.start(
 	:app => builder,
 	:Port => 8080,
-  :server => 'thin',
-  :host => 'www.example.com'
+  :server => 'thin'
 )
 
 
